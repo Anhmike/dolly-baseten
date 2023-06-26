e621a4f37882c47f7b48b07e0e6ce5eb44b25f8c
Mike <Anh.mike.pham@gmail.com>
2023-06-26 15:34:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import truss
+import baseten
 
+dolly_v2_truss = truss.load("path/to/dolly_v2_truss")
+
+baseten.login("PASTE_API_KEY_HERE")
+
+baseten.deploy(dolly_v2_truss, model_name="Dolly-v2", publish=True)
+
+
+model = baseten.deployed_model_version_id('YOUR_MODEL_ID')
+
+request = {
+    "prompt": "Explain to me the difference between nuclear fission and fusion."
+}
+
+response = model.predict(request)
